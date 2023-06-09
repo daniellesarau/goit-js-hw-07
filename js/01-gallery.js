fb6cedda1e8cc28eac4d5462a11aa2a3e7e7dc8e
daniellesarau <123309317+daniellesarau@users.noreply.github.com>
2023-06-09 16:37:55 +0300

    final change

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+console.log(galleryItems);
+
+const galleryElement = document.querySelector(".gallery");
+
+const makeItems = ({ preview, original, description }) =>
+ `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+const markup = galleryItems.map((element) => makeItems(element)).join("");
+
+galleryElement.insertAdjacentHTML("afterbegin", markup);
+
+galleryElement.addEventListener("click", (event) => {
+ event.preventDefault();
+
+ if (event.target.nodeName !== "IMG") {
+  return;
+ }
+
+ const modalPicture = event.target.dataset.source;
+
+ const instance = basicLightbox.create(`
+    <img src="${modalPicture}" width="800" height="600">`);
+ instance.show();
+});
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const galleryElement = document.querySelector(".gallery");
+
+const makeItems = ({ preview, original, description }) =>
+ `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+const markup = galleryItems.map((element) => makeItems(element)).join("");
+
+galleryElement.insertAdjacentHTML("afterbegin", markup);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+ captionsData: "alt",
+ captionPosition: "bottom",
+ captionDelay: 250,
+ close: false,
+});
 console.log(galleryItems);
